80f542283c46d074568b5cd2adca37a42cdfd62f
Brad Simantel <brad@theprogrampdx.com>
2013-11-11 11:30:37 -0800

    Added makeGeoJSON() and makeMap(), app now puts points on map

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 (function() {
 
-  // initialize map, add tile layer
-  var map = L.map('map').setView([51.505, -0.09], 13);
-  L.tileLayer('http://{s}.tile.cloudmade.com/API-key/997/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
-  }).addTo(map);
-
   // source for CSVToArray and CSV2JSON: http://www.bennadel.com/blog/1504-Ask-Ben-Parsing-CSV-Strings-With-Javascript-Exec-Regular-Expression-Command.htm
   function CSVToArray(strData, strDelimiter) {
     strDelimiter = (strDelimiter || ",");
@@ -48,25 +41,51 @@
     return str;
   }
 
-  // stick JSON in map
-  function addToMap(json) {
-    console.log(json);
+  // make map with geoJSON
+  function makeMap(geojson) {
+    var geojson = L.geoJson(geojson);
+    var map = L.map('map').fitBounds(geojson.getBounds());
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    geojson.addTo(map);
   }
 
-  // geocode each record (assumes you had "Address", "City", "State", and "ZIP" columns in CSV)
+  // make valid geoJSON (from points), add to map
+  // assumes that the "location" key we just added to JSON is the only "location" key
+  // which could be false if the user had a "location" row in spreadsheet originally
+  function makeGeoJSON(json) {
+    var geojson = {"type": "FeatureCollection", "features": []}
+    $.each(json, function(index, record) {
+      var feature = {
+        "type": "Feature",
+        "properties": record,
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            record.location.lng,
+            record.location.lat
+          ]
+        }
+      };
+      geojson.features.push(feature);
+    });
+    makeMap(geojson);
+  }
+
+  // geocode each record (assumes you had "Address", "City", "State", and "ZIP" columns in CSV for right now)
   // eventually I want to try to guess which field is which, only ask the user if uncertain/to verify
   // I will have to ask which row, if any, contains the quantitative/qualititative data
   function geocode(json) {
     var json = $.parseJSON(json);
-    $.each(json, function(index, record) {
+    $.when.apply($, $.map(json, function(record, index) {
       var url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=" + record.Address + ", " + record.City + ", " + record.State + " " + record.ZIP
-      $.getJSON(url, function(data) {
-        $.extend(json[index], data.results[0].geometry.location);
+      return $.getJSON(url, function(data) {
+        $.extend(json[index], data.results[0].geometry);
       });
+    })).done(function() {
+      makeGeoJSON(json);
     });
-    // send json (now with lat and lng as keys) off to be mapped
-    // ACTUALLY, this has to be geojson, so let's convert this shit first (or do it back on line 64)
-    addToMap(json)
   }
 
   // handle upload of CSV (and eventually XLS/XLSX with https://github.com/SheetJS/js-xls and https://github.com/SheetJS/js-xlsx)
@@ -76,8 +95,8 @@
     reader.onload = function(e) {
       var csv = e.target.result;
       var json = CSV2JSON(csv);
-      // eventually detect if there's an address to geocode, otherwise assume we're joining the data to our geometry
-      // then guess by comparing to list of countries, states (or equivalent for other countries), and counties (or equivalent for other countries)
+      // eventually I want to detect if there's an address to geocode, otherwise I'll assume we're joining the data to geometry for counties/states/countries
+      // and try to figure out which it is, do that automatically
       geocode(json);
     }
     reader.onerror = function(e) {
