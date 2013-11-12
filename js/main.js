2dbe11e3945a078dfe89bc3e65f9a8ea715891fa
Brad Simantel <brad@theprogrampdx.com>
2013-11-12 15:42:51 -0800

    Moved around TODOs, added a couple options for findAddressFields

--- js/main.js
+++ js/main.js
@@ -66,8 +66,12 @@
   }
 
   // Make map and add geoJSON
+  // TODO: We will have to ask which row, if any, contains the quantitative/qualititative data to group by/symbolize.
   // TODO: Symbolize data based on quantitative/qualitative data properties.
   // TODO: Show all feature properties in popup.
+  // TODO: Add ability to search for points within a given distance of user input.
+  // TODO: Allow filtering of data on the map.
+  // TODO: Add an option to fix our assumptions about fields and geocode again, join to different geometry, or address failed geocodes.
   function makeMap(geojson) {
     var geojson = L.geoJson(geojson, {
       onEachFeature: function(feature, layer) {
@@ -83,6 +87,8 @@
 
   // Make valid geoJSON
   // TODO: Don't assume that the "location" key we just added to JSON is the only "location" key.
+  // TODO: Allow for other lat/long to be in places we didn't put it (if passed directly from parseFields).
+  // TODO: Remove "location" property from "properties" in geoJSON.
   function makeGeoJSON(json) {
     var geojson = {"type": "FeatureCollection", "features": []}
     $.each(json, function(index, record) {
@@ -103,12 +109,11 @@
   }
 
   // Geocode each record
-  // TODO: Don't depend on test data. Right now we assume the table had "Address", "City", "State", and "ZIP" columns.
-  // TODO: Instead of requiring user input, guess which field is which, only ask the user if uncertain/to verify.
-  // TODO: We will have to ask which row, if any, contains the quantitative/qualititative data to group by/symbolize.
+  // TODO: Put failed geocodes somewhere that the user can see and edit them/try to geocode them again.
+  // TODO: Remove geocode property from JSON after we add the location property.
   function geocode(json) {
     $.when.apply($, $.map(json, function(record, index) {
-      var url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=" + record.Address + ", " + record.City + ", " + record.State + " " + record.ZIP
+      var url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=" + record.geocode;
       return $.getJSON(url, function(data) {
         $.extend(json[index], data.results[0].geometry);
       });
@@ -117,9 +122,66 @@
     });
   }
 
+  // Take JSON and attach it to appropriate country/state/county/ZIP/area code geoJSON
+  // TODO: Everything.
+  function joinToGeometry(json) {
+    return json;
+  }
+
+  // Try to identify address fields from user's spreadsheet so we don't have to ask them before geocoding
+  // TODO: Concatenate all the matched keys' values and put them in a "geocode" property in the JSON.
+  // TODO: Once this function is working properly, roll it into parseFields.
+  function findAddressFields(json) {
+    var keys = Object.keys(json[0]);
+    var potentialKeys = ["address", "addr", "add", "city", "state", "zip", "zip code", "zipcode"];
+    var matches = {};
+
+    var keyMap = {address: ["address", "addr", "add"],
+                  zipcode: ["zipcode", "zip code"]};
+
+    record = {AddreSS: "Kevins Place", ZiPcode: "12341"};
+
+    findProperty = function(type, record) {
+      return _.find(record, function(val, key) {
+        keyOpportunities = (keyMap[type]);
+        return _.find(keyOpportunities, function(keyOption) {
+          return(keyOption === key.toLowerCase());
+        })
+      })
+    }
+
+    findProperty("address", record);
+
+    // $.each(keys, function(index, key) {
+    //   keysPositionInPotentialKeys = $.inArray(key.toLowerCase(), potentialKeys);
+    //   if (keysPositionInPotentialKeys !== -1) {
+    //     var potentialKeyInQuestion = potentialKeys[keysPositionInPotentialKeys];
+    //     matches[potentialKeyInQuestion] = index;
+    //   }
+    // });
+    // console.log(matches);
+    // $.each(json, function(index, record) {
+    //   console.log(record);
+    //   // if (matches.address || matches.city) {
+    //   //   record.geocode = record[matches.address] + ", " + record.[matches.city] + ", " + record.[matches.city] + " " + record.[matches.zip];
+    //   // }
+    // });
+    // geocode(json);
+  }
+
+  // Parse fields, send JSON on to appropriate function
+  // TODO: Make this function actually work.
+  function parseFields(json) {
+    findAddressFields(json);
+  //   var keys = Object.keys(json[0]);
+  //   else if keys contains lat/lng -> makeGeoJSON(json)
+  //   else if keys contains address -> geocode(json)
+  //   else if keys contains city -> geocode(json)
+  //   else if keys contains state, zip, country, phone number, or county -> joinToGeometry(json)
+  //   else -> ask the user to specify which field is which
+  }
+
   // Handle upload of CSV/XLS/XLSX
-  // TODO: Fix XLSX handling - currently not working.
-  // TODO: Detect if there's an address to geocode, otherwise assume we're joining the data to geometry for counties/states/countries, detect which it may be, and do that automatically.
   // TODO: Either combine all sheets in XLS and XLSX files or ask user to choose a sheet from list of sheets with content.
   $('#fileInput').change(function(event) {
     var file = event.target.files[0];
@@ -130,7 +192,7 @@
         var csv = e.target.result;
         var json = csvToJSON(csv);
         json = $.parseJSON(json);
-        geocode(json);
+        parseFields(json);
       }
       reader.readAsText(file);
     }
@@ -140,7 +202,7 @@
         var cfb = XLS.CFB.read(xls, {type: 'binary'});
         var wb = XLS.parse_xlscfb(cfb);
         var json = xlsToJSON(wb);
-        geocode(json.Sheet1);
+        parseFields(json.Sheet1);
       }
       reader.readAsBinaryString(file);
     }
@@ -150,7 +212,7 @@
         var arr = String.fromCharCode.apply(null, new Uint8Array(xlsx));
         var wb = XLSX.read(btoa(arr), {type: 'base64'});
         var json = xlsxToJSON(wb);
-        geocode(json.Sheet1);
+        parseFields(json.Sheet1);
       }
       reader.readAsArrayBuffer(file);
     }
