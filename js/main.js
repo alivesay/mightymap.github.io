858e71f584c55abf7bce58cd10bb5e5277bca956
Brad Simantel <brad@theprogrampdx.com>
2013-11-11 16:43:07 -0800

    Added popups, indentation fix, comments

--- js/main.js
+++ js/main.js
@@ -67,8 +67,13 @@
 
   // Make map and add geoJSON
   // TODO: Symbolize data based on quantitative/qualitative data properties.
+  // TODO: Show all feature properties in popup.
   function makeMap(geojson) {
-    var geojson = L.geoJson(geojson);
+    var geojson = L.geoJson(geojson, {
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup(feature.properties.Address);
+      }
+    });
     var map = L.map('map').fitBounds(geojson.getBounds());
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -102,7 +107,6 @@
   // TODO: Instead of requiring user input, guess which field is which, only ask the user if uncertain/to verify.
   // TODO: We will have to ask which row, if any, contains the quantitative/qualititative data to group by/symbolize.
   function geocode(json) {
-    console.log(json);
     $.when.apply($, $.map(json, function(record, index) {
       var url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=" + record.Address + ", " + record.City + ", " + record.State + " " + record.ZIP
       return $.getJSON(url, function(data) {
@@ -114,49 +118,45 @@
   }
 
   // Handle upload of CSV/XLS/XLSX
+  // TODO: Fix XLSX handling - currently not working.
   // TODO: Detect if there's an address to geocode, otherwise assume we're joining the data to geometry for counties/states/countries, detect which it may be, and do that automatically.
-  // TODO: Either combine all sheets in XLS and XLSX files or ask user to choose a sheet (from list of sheets with content).
+  // TODO: Either combine all sheets in XLS and XLSX files or ask user to choose a sheet from list of sheets with content.
   $('#fileInput').change(function(event) {
     var file = event.target.files[0];
     var reader = new FileReader()
     var regex = /\.[0-9a-z]+$/i;
-
     if (regex.exec(file.name)[0].toLowerCase() === ".csv") {
       reader.onload = function(e) {
-          var csv = e.target.result;
-          var json = csvToJSON(csv);
-          json = $.parseJSON(json);
-          geocode(json);
+        var csv = e.target.result;
+        var json = csvToJSON(csv);
+        json = $.parseJSON(json);
+        geocode(json);
       }
-      reader.readAsBinaryString(file);
+      reader.readAsText(file);
     }
-
     else if (regex.exec(file.name)[0].toLowerCase() === ".xls") {
       reader.onload = function(e) {
-          var xls = e.target.result;
-          var cfb = XLS.CFB.read(xls, {type: 'binary'});
-          var wb = XLS.parse_xlscfb(cfb);
-          var json = xlsToJSON(wb);
-          geocode(json.Sheet1);
+        var xls = e.target.result;
+        var cfb = XLS.CFB.read(xls, {type: 'binary'});
+        var wb = XLS.parse_xlscfb(cfb);
+        var json = xlsToJSON(wb);
+        geocode(json.Sheet1);
       }
       reader.readAsBinaryString(file);
     }
-
     else if ((regex.exec(file.name)[0].toLowerCase() === ".xlsx")) {
       reader.onload = function(e) {
-          var xlsx = e.target.result;
-          var arr = String.fromCharCode.apply(null, new Uint8Array(xlsx));
-          var wb = XLSX.read(btoa(arr), {type: 'base64'});
-          var json = xlsxToJSON(wb);
-          geocode(json.Sheet1);
+        var xlsx = e.target.result;
+        var arr = String.fromCharCode.apply(null, new Uint8Array(xlsx));
+        var wb = XLSX.read(btoa(arr), {type: 'base64'});
+        var json = xlsxToJSON(wb);
+        geocode(json.Sheet1);
       }
-      reader.readAsBinaryString(file);
+      reader.readAsArrayBuffer(file);
     }
-
     else {
       console.log("The file you're uploading isn't a CSV, XLS, or XLSX.");
     }
-
   });
 
 })();
