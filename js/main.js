35d08c71b9c956a351d75035a0c5a0b546e15c81
Brad Simantel <brad@theprogrampdx.com>
2013-11-12 16:16:44 -0800

    Removed one of the options, am now sort of parsing for various field combinations

--- js/main.js
+++ js/main.js
@@ -88,7 +88,7 @@
   // Make valid geoJSON
   // TODO: Don't assume that the "location" key we just added to JSON is the only "location" key.
   // TODO: Allow for other lat/long to be in places we didn't put it (if passed directly from parseFields).
-  // TODO: Remove "location" property from "properties" in geoJSON.
+  // TODO: Remove location property from properties in geoJSON.
   function makeGeoJSON(json) {
     var geojson = {"type": "FeatureCollection", "features": []}
     $.each(json, function(index, record) {
@@ -125,60 +125,50 @@
   // Take JSON and attach it to appropriate country/state/county/ZIP/area code geoJSON
   // TODO: Everything.
   function joinToGeometry(json) {
-    return json;
+    return true;
   }
 
-  // Try to identify address fields from user's spreadsheet so we don't have to ask them before geocoding
-  // TODO: Concatenate all the matched keys' values and put them in a "geocode" property in the JSON.
-  // TODO: Once this function is working properly, roll it into parseFields.
-  function findAddressFields(json) {
-    var keys = Object.keys(json[0]);
-    var potentialKeys = ["address", "addr", "add", "city", "state", "zip", "zip code", "zipcode"];
-    var matches = {};
+  // Parse fields so we don't have to ask user which is which, send JSON on to appropriate function
+  // TODO: For address and city cases (the ones we're geocoding), I need to only attach other properties which exist for geocoding.
+  function parseFields(json) {
+    var sampleRecord = json[0]
+    var keyMap = {
+      address: ["address", "addr", "add"],
+      city: ["city"],
+      state: ["state"],
+      zip: ["zip", "zipcode", "zip code"],
+      county: ["county"],
+      country: ["country"],
+      latitude: ["lat", "latitude"],
+      longitude: ["lng", "long", "longitude"]
+    };
 
-    var keyMap = {address: ["address", "addr", "add"],
-                  zipcode: ["zipcode", "zip code"]};
-
-    record = {AddreSS: "Kevins Place", ZiPcode: "12341"};
-
-    findProperty = function(type, record) {
+    function findProperty(type, record) {
       return _.find(record, function(val, key) {
         keyOpportunities = (keyMap[type]);
         return _.find(keyOpportunities, function(keyOption) {
           return(keyOption === key.toLowerCase());
-        })
-      })
+        });
+      });
     }
 
-    findProperty("address", record);
-
-    // $.each(keys, function(index, key) {
-    //   keysPositionInPotentialKeys = $.inArray(key.toLowerCase(), potentialKeys);
-    //   if (keysPositionInPotentialKeys !== -1) {
-    //     var potentialKeyInQuestion = potentialKeys[keysPositionInPotentialKeys];
-    //     matches[potentialKeyInQuestion] = index;
-    //   }
-    // });
-    // console.log(matches);
-    // $.each(json, function(index, record) {
-    //   console.log(record);
-    //   // if (matches.address || matches.city) {
-    //   //   record.geocode = record[matches.address] + ", " + record.[matches.city] + ", " + record.[matches.city] + " " + record.[matches.zip];
-    //   // }
-    // });
-    // geocode(json);
-  }
-
-  // Parse fields, send JSON on to appropriate function
-  // TODO: Make this function actually work.
-  function parseFields(json) {
-    findAddressFields(json);
-  //   var keys = Object.keys(json[0]);
-  //   else if keys contains lat/lng -> makeGeoJSON(json)
-  //   else if keys contains address -> geocode(json)
-  //   else if keys contains city -> geocode(json)
-  //   else if keys contains state, zip, country, phone number, or county -> joinToGeometry(json)
-  //   else -> ask the user to specify which field is which
+    if (findProperty("latitude", sampleRecord) && findProperty("longitude", sampleRecord)) {
+      makeGeoJSON(json)
+    } else if (findProperty("address", sampleRecord)) {
+      $.each(json, function(index, record) {
+        record.geocode = findProperty("address", record) + ", " + findProperty("city", record) + ", " + findProperty("state", record) + " " + findProperty("zip", record);
+      });
+      geocode(json);
+    } else if (findProperty("city", sampleRecord)) {
+      $.each(json, function(index, record) {
+        record.geocode = findProperty("city", record) + ", " + findProperty("state", record)
+      });
+      geocode(json);
+    } else if (findProperty("state", sampleRecord) || (findProperty("zip", sampleRecord) || (findProperty("country", sampleRecord) || (findProperty("county", sampleRecord)) {
+      joinToGeometry(json);
+    } else {
+      console.log("This is where we should be showing dropdowns to let the user specify which field is which.")
+    }
   }
 
   // Handle upload of CSV/XLS/XLSX
@@ -195,8 +185,7 @@
         parseFields(json);
       }
       reader.readAsText(file);
-    }
-    else if (regex.exec(file.name)[0].toLowerCase() === ".xls") {
+    } else if (regex.exec(file.name)[0].toLowerCase() === ".xls") {
       reader.onload = function(e) {
         var xls = e.target.result;
         var cfb = XLS.CFB.read(xls, {type: 'binary'});
@@ -205,8 +194,7 @@
         parseFields(json.Sheet1);
       }
       reader.readAsBinaryString(file);
-    }
-    else if ((regex.exec(file.name)[0].toLowerCase() === ".xlsx")) {
+    } else if ((regex.exec(file.name)[0].toLowerCase() === ".xlsx")) {
       reader.onload = function(e) {
         var xlsx = e.target.result;
         var arr = String.fromCharCode.apply(null, new Uint8Array(xlsx));
@@ -215,8 +203,7 @@
         parseFields(json.Sheet1);
       }
       reader.readAsArrayBuffer(file);
-    }
-    else {
+    } else {
       console.log("The file you're uploading isn't a CSV, XLS, or XLSX.");
     }
   });
