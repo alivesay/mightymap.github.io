5e17608d9696dddded8df1728d480a8d38178b0d
Brad Simantel <brad@theprogrampdx.com>
2014-01-09 17:53:30 -0800

    Moved JS and CSS to _includes, compiling into all.css/all.js

--- _site/public/js/main.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  
-  // Instantiate model, views.
-  window.map = new window.mapModel();
-  window.uploadHandler = new window.uploadHandlerView({model: window.map});
-  window.selectFields = new window.selectFieldsView({model: window.map});
-  window.mapMaker = new window.mapMakerView({model: window.map});
-
-})();
---
+++ public/js/all.js
@@ -0,0 +1,16 @@
+---
+---
+
+{% include js/vendor/jquery.js %}
+{% include js/vendor/underscore.js %}
+{% include js/vendor/backbone.js %}
+{% include js/vendor/jszip.js %}
+{% include js/vendor/xls.js %}
+{% include js/vendor/xlsx.js %}
+{% include js/vendor/shp.js %}
+{% include js/vendor/plugins.js %}
+{% include js/models/map.js %}
+{% include js/views/upload-handler.js %}
+{% include js/views/select-fields.js %}
+{% include js/views/map-maker.js %}
+{% include js/main.js %}
